0f829649a8b5ceea78aef9f9e54d6ec841f6b371
oukichikou <oukichikou@192.168.110.79>
2019-08-30 09:14:20 +0800

    11

---
+++ recode/src/main/java/controller/testController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+	@RequestMapping("index")
+	public void index(String txt,ModelMap m) {
+		m.put("info", txt);
+	}
+	
+}
