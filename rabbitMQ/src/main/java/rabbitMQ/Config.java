842a39ab734561e6e280a786e150c7e1213c4de9
oukichikou <oukichikou@192.168.110.79>
2019-09-11 09:36:05 +0800

    RABBIT

---
+++ rabbitMQ/src/main/java/rabbitMQ/Config.java
@@ -0,0 +1,10 @@
+package rabbitMQ;
+
+public class Config {
+public final static String QueueName="my";
+public static final String UserName = "guest";
+public static final String Password = "guest";
+public static final int Port = 5672;
+public static final String Host = "127.0.0.1";
+public static final String VHost = "/";
+}
